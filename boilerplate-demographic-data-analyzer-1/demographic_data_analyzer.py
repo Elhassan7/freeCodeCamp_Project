ce1f42be69fef6dbe3378c5ad3a4e43767339be5
Elhassan7 <chakouki.el.hassan@gmail.com>
2022-03-14 22:33:09 +0100

    git push

--- boilerplate-demographic-data-analyzer-1/demographic_data_analyzer.py
+++ boilerplate-demographic-data-analyzer-1/demographic_data_analyzer.py
@@ -3,13 +3,13 @@ import pandas as pd
 
 def calculate_demographic_data(print_data=True):
     # Read data from file
-    df = None
+    df = pd.read_csv("adult.data.csv")
 
     # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
-    race_count = None
-
+    race_count = df.groupby("race").count()["age"]
+    
     # What is the average age of men?
-    average_age_men = None
+    average_age_men = round(df[df["sex"]=="Male"].age.mean(), 10)
 
     # What is the percentage of people who have a Bachelor's degree?
     percentage_bachelors = None
