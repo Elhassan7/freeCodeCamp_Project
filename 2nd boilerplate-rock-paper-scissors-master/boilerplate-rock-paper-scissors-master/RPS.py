08f845624688ed6f8772fe9f578e3c67622c80cb
Elhassan7 <chakouki.el.hassan@gmail.com>
2022-06-17 00:09:20 +0100

    git push

--- 2nd boilerplate-rock-paper-scissors-master/boilerplate-rock-paper-scissors-master/RPS.py
+++ 2nd boilerplate-rock-paper-scissors-master/boilerplate-rock-paper-scissors-master/RPS.py
@@ -1,10 +1,42 @@
 # The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
 
+
+steps = {}
+
 def player(prev_play, opponent_history=[]):
-    opponent_history.append(prev_play)
+    if prev_play != "":
+        opponent_history.append(prev_play)
+
+    n = 2
+
+    hist = opponent_history
 
     guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+    if len(hist) > n:
+        pattern = join(hist[-n:])
+
+        if join(hist[-(n + 1):]) in steps.keys():
+            steps[join(hist[-(n + 1):])] += 1
+        else:
+            steps[join(hist[-(n + 1):])] = 1
+
+        possible = [pattern + "R", pattern + "P", pattern + "S"]
+
+        for i in possible:
+            if not i in steps.keys():
+                steps[i] = 0
+
+        predict = max(possible, key=lambda key: steps[key])
+
+        if predict[-1] == "P":
+            guess = "S"
+        if predict[-1] == "R":
+            guess = "P"
+        if predict[-1] == "S":
+            guess = "R"
 
     return guess
+
+
+def join(moves):
+    return "".join(moves)
